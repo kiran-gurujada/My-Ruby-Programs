68d8b31c1ae015a71401b571e014ce2df0577b6a
Kiran-gurujada <kiran@gurujada.org>
2017-01-04 21:40:13 +0530

    Added buuble_sort program file for sorting items in an array

---
+++ bubble_sort.rb
@@ -0,0 +1,31 @@
+
+public
+
+def bubble_sort
+	
+	n = self.length
+	
+	loop do
+		swapped = false
+	
+		(n-1).times do |i|
+
+			if self[i] > self[i+1]
+				self[i], self[i+1] = self[i+1], self[i]
+				swapped = true
+			end
+
+		end
+		
+		break if not swapped	
+	end
+
+	self
+
+end
+
+
+array = [4, 2, 3, 7, 6, 8, 5, 10, 1, 9]
+
+p array.bubble_sort
+
