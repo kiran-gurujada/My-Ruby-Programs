244c82676dd546d966d87483b46786fd18e42a46
Kiran Kumar Chirravuri <kiran@gurujada.org>
2017-01-05 10:28:52 +0530

    created a special directory Euler Problems to solve some problems listed at projecteuler.net

---
+++ Euler Problems/problem1.rb
@@ -0,0 +1,17 @@
+# Sum of the multiples of 3 and 5 below 1000
+
+array = (0..999).to_a
+
+result_array = []
+
+array.each do |i|
+	
+	if i % 3 == 0 || i % 5 == 0
+		result_array << i		
+	end
+
+	result_array
+
+end
+
+p result_array
